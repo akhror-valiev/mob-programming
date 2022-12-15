4f15f96066ba9355a18c51a45fc98c10beeb0918
akhror-valiev <ahror.valiev@gmail.com>
2022-12-15 12:04:48 +0500

    Recursion problem solving:Factorial

---
+++ challenge.js
@@ -0,0 +1,16 @@
+function sum(number) {
+  if (number > 0) {
+    return number + sum(number - 1);
+  }
+  return number;
+
+
+}
+
+
+console.log(sum(4));
+console.log(sum(10));
+
+
+module.exports = sum
+
