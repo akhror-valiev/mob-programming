349bbdbd2fd7399af43b3a0abe66e51f1f9a5954
akhror-valiev <ahror.valiev@gmail.com>
2023-03-15 10:51:25 +0500

    Capitalize first char of the every text

---
+++ capitalizeString.js
@@ -0,0 +1,6 @@
+const capitalizeString = str => {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+console.log(capitalizeString("ahror"))
+console.log(capitalizeString("you are welcome"))
