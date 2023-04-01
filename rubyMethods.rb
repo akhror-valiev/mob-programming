499e967237f1d0e32416a2cfa2fbb3af3f3bcb59
akhror-valiev <ahror.valiev@gmail.com>
2023-04-01 12:11:48 +0500

    Ruby methods

---
+++ rubyMethods.rb
@@ -0,0 +1,17 @@
+# Uniq
+
+numbers = [1,2,3,4,2,6,3]
+
+unique_numbers = numbers.uniq  # => [1,2,3,4,6]
+
+# Sort
+numbers = [5,3,1,4,2]
+
+sorted_numbers = numbers.sort # => [1,2,3,4,5]
+
+
+# Split
+sentence = "Ruby on Rails is awesome"
+
+words = sentence.split(" ") # => ["Ruby", "on", "Rails", "is", "owesome"]
+
